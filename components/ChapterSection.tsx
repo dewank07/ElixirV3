5cf2a1659c732f798e63a8f219276fb5d6753187
Akash-1808 <akashch1808@gmail.com>
2025-01-04 22:01:47 +0530

    change-Link-tag

--- components/ChapterSection.tsx
+++ components/ChapterSection.tsx
@@ -4,6 +4,7 @@ import { Chapters } from "@/data";
 import { PinContainer } from "./ui/Pin";
 import Image from "next/image";
 import Link from "next/link";
+import Link from "next/link";
 
 const ChapterSection = () => {
   return (
@@ -17,7 +18,7 @@ const ChapterSection = () => {
             className="h-auto w-64 flex items-center justify-center"
             key={idx}
           >
-            <a href={item.link} target="_blank">
+            <Link href={item.link} target="_blank">
               <PinContainer title={item.title} href={item.link}>
                 <div
                   className={`flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 ${
@@ -43,7 +44,7 @@ const ChapterSection = () => {
                   </div>
                 </div>
               </PinContainer>
-            </a>
+            </Link>
           </div>
         ))}
       </div>
