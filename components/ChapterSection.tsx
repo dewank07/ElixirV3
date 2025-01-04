834d0bf9760b7cf082e9a7172409396851852b86
Akash-1808 <akashch1808@gmail.com>
2025-01-04 22:01:47 +0530

    change-Link-tag

--- components/ChapterSection.tsx
+++ components/ChapterSection.tsx
@@ -3,6 +3,7 @@
 import { Chapters } from "@/data";
 import { PinContainer } from "./ui/Pin";
 import Image from "next/image";
+import Link from "next/link";
 
 const ChapterSection = () => {
   return (
@@ -13,7 +14,8 @@ const ChapterSection = () => {
       <div className='flex flex-wrap items-center justify-center p-4 gap-20 mt-10'>
         {Chapters.map((item, idx) => (
           <div className='h-auto w-64 flex items-center justify-center' key={idx}>
-            <a href={item.link} target="_blank"><PinContainer title={item.title} href={item.link}>
+            <Link href={item.link} target="_blank">
+              <PinContainer title={item.title} href={item.link}>
               <div
                 className={`flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 ${
                   idx == 1 ? "md:w-[18rem] md:h-[22rem] w-[16rem] h-[20rem]" : " w-[16rem] h-[20rem]"
@@ -34,7 +36,8 @@ const ChapterSection = () => {
                   />
                 </div>
               </div>
-            </PinContainer></a>
+            </PinContainer>
+            </Link>
           </div>
           
         ))}
