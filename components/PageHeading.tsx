5b0cfbb6031e6d7a60667bdd1665433d005619be
PrakharJain345 <jkavita357@gmail.com>
2025-01-18 23:00:22 +0530

    Removed section tab which caused redundant links

--- components/PageHeading.tsx
+++ components/PageHeading.tsx
@@ -2,6 +2,7 @@ import React, { memo } from "react";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const PageHeading = ({
+  smallHeading,
   longHeading,
 }: {
   smallHeading?: string;
@@ -9,8 +10,12 @@ const PageHeading = ({
   link?: string;
 }) => {
   return (
-    <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-      <TextGenerateEffect words={longHeading} className='text-center text-[20px] md:text-2xl ' />
+    <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+      {/* <AnimatedShinyTextDemo text={smallHeading} link={link} /> */}
+      <TextGenerateEffect
+        words={longHeading}
+        className="text-center text-[20px] md:text-2xl "
+      />
     </div>
   );
 };
