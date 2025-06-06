ba90358aff224568a75f7c012731b3c73f7d056d
Vansh Gupta <vanshgupta9917@gmail.com>
2025-06-06 18:31:24 +0530

    Re-designed Mentor Page

--- layouts/MentorPageLayout.tsx
+++ layouts/MentorPageLayout.tsx
@@ -9,19 +9,23 @@ const MentorPageLayout = () => {
   const [mentorData, setMentorData] = useState<typeof MentorData>(MentorData);
 
   return (
-    <div className="pt-20 relative z-10">
+    <div className="pt-20 pb-10 relative z-10 w-full">
       <div className="flex flex-col items-center justify-center">
         <PageHeading longHeading="REACH OUT FOR GUIDANCE" />
-        {mentorData.length > 0 && (
-          <SearchBar setMentorData={setMentorData} mentors={mentorData} />
-        )}
+        <SearchBar setMentorData={setMentorData} mentors={mentorData} />
         <div
           className="flex flex-wrap items-center justify-center"
           style={{ gap: "80px" }}
         >
-          {mentorData.map((mentor, idx) => (
-            <MentorCard key={idx} data={mentor} />
-          ))}
+          {mentorData?.length === 0 ? (
+            <div className="flex justify-center items-center text-3xl font-bold w-[320px] py-20 rounded-3xl overflow-hidden bg-black">
+              <p className="mb-4 text-2xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">No mentors found</p>
+            </div>
+          ) : (
+            mentorData?.map((mentor, idx) => (
+              <MentorCard key={idx} data={mentor} />
+            ))
+          )}
         </div>
       </div>
     </div>
